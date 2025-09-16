449123670490efbb39653c025cc2dc998b40fbc5
oktab <aslbekxayrullayev1@gmail.com>
2025-09-16 13:53:23 +0300

    ustozdan ovomman

---
+++ src/Components/HomepageView/HomepageView.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function HomepageView() {
+    return (
+        <>
+            <div className="bg-[#2942A5]">
+                <div className="max-w-[1240px] mx-auto pt-[80px]">
+                    <div className="">
+                        <div className=""></div>
+                        <input type="text" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default HomepageView
